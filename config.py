c5c808dd6531a014b47cee383b0d161b8c048c66
tejausha <tejausha@outlook.com>
2024-12-16 18:18:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7668066184:AAHfI8qkVjUNk_N5STZFZhciOc-65ILNHIk")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@BKMOMNIVERSE")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1232759277")
+
+
+
+
+
+
+
+  
